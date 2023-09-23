7d1a3324f0a50051d900d6f7a5dd3b3dabf65915
dostendite <dostendite@outlook.es>
2023-09-23 23:58:49 +0200

    Fix caesar_cipher function
    Program's finished & works properly,
    it uses regex to filter non-alphabetic
    characters, is case-sensitive and allows
    for shift factor & index pairs higher than 26

--- caesar-cipher/caesar_cipher.rb
+++ caesar-cipher/caesar_cipher.rb
@@ -15,11 +15,15 @@ def caesar_cipher(string, shift_factor)
   new_string = []
 
   string.chars.each do |char|
-    if char =~ /[ !?.,]/
+    if char =~ /[\W\d]/
       new_string << char
+    elsif char == char.upcase # find capital
+        alphabet_index = alphabet.find_index(char.downcase)
+        new_string << alphabet[(alphabet_index + shift_factor) % 26].upcase
     else
-      alphabet_index = alphabet.find_index(char)
-      new_string << alphabet[(alphabet_index + shift_factor) % 26]
+        alphabet_index = alphabet.find_index(char)
+        new_string << alphabet[(alphabet_index + shift_factor) % 26]
+    end
   end
   new_string.join("")
 end
