250ac80d7ae1567a77238537b79a0f4638db57e4
dostendite <dostendite@outlook.es>
2023-09-24 21:19:13 +0200

    Implement fully working solution
    Fixed the bug where it would return the
    first occurrence of compare_number in the
    array instead of just passing the index of
    the number itself without a method.

--- stock-picker/stock_picker.rb
+++ stock-picker/stock_picker.rb
@@ -1,23 +1,43 @@
-stock_prices = [17,3,6,9,15,8,6,1,10]
+stock_prices = [60, 14, 5, 3, 7 , 8, 60, 32, 4, 1]
 
 def stock_picker(prices_array)
   
   # Buy day & Sell day
-  best_deal = [0, 0]
+  best_deal = {
+    value: 0,
+    buy_day: 0,
+    sell_day: 0
+  }
 
   # Loop through the array
-
   # Loop again to compare the
   # current number with the others
 
-  # If the current number minus the
-  # number compared against it
-  # return higher than best_deal,
-  # best deal[0] = current_number
-  # best_deal[1] = compare_number
+  # If the number compared against
+  # the current number returns 
+  # higher than deal_value,
+  # add them to the hash
 
-  # Then return best_deal
+  # Then return best_deal_days
+  prices_array.each_with_index do |current_num, current_index|
+    prices_array.each_with_index do |compare_num, compare_index|
 
-days = stock_picker(stock_prices)
+      if compare_index > current_index
+        if ((compare_num - current_num) > best_deal[:value])
+          best_deal[:value] = (compare_num - current_num)
+          best_deal[:buy_day] = current_index + 1
+          best_deal[:sell_day] = compare_index + 1
+        end
+      end
+    end
+  end
 
-puts days
+  best_deal
+end
+
+best_deal = stock_picker(stock_prices)
+
+puts "BEST DEAL:\n~~~~~~~~~~~~~~~~
+Buy on day #{best_deal[:buy_day]}
+Sell on day #{best_deal[:sell_day]}
+Profit: #{best_deal[:value]}$\n~~~~~~~~~~~~~~~~"
