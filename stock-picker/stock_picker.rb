169505644e671647de10c4a4c145c80475352fda
dostendite <dostendite@outlook.es>
2023-09-24 20:53:07 +0200

    Plan & pseudocode second hypothesis

--- stock-picker/stock_picker.rb
+++ stock-picker/stock_picker.rb
@@ -1,14 +1,23 @@
 stock_prices = [17,3,6,9,15,8,6,1,10]
 
 def stock_picker(prices_array)
+  
+  # Buy day & Sell day
+  best_deal = [0, 0]
 
-  # find lowest value
+  # Loop through the array
 
-  # find highest value
+  # Loop again to compare the
+  # current number with the others
 
-  # return index of lowest and highest values
-end
+  # If the current number minus the
+  # number compared against it
+  # return higher than best_deal,
+  # best deal[0] = current_number
+  # best_deal[1] = compare_number
+
+  # Then return best_deal
 
 days = stock_picker(stock_prices)
 
-p days
+puts days
