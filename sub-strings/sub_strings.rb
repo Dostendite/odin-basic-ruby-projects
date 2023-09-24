b96922d87ad47038a3296c75e769428e51d29c2b
dostendite <dostendite@outlook.es>
2023-09-24 12:11:44 +0200

    Implement fully working solution
    The new solution iterates over all the
    words in the dictionary and counts all the
    regex occurrences for that specific word in the
    string given by the user, and then assigns
    the number of matches to the hash value for the
    matched word key.

--- sub-strings/sub_strings.rb
+++ sub-strings/sub_strings.rb
@@ -1,33 +1,17 @@
-require 'pry-byebug'
-# Implement a method #substrings that takes a 
-# word as the first argument and then an array of 
-# valid substrings (your dictionary) as the second argument. 
-# It should return a hash listing each substring (case insensitive) 
-# that was found in the original string and  how many times it was found.
-
 print "Enter input string: "
 input_string = gets.chomp
+
 dictionary = ["below","down","go","going","horn","how","howdy",
               "it","i","low","own","part","partner","sit"]
-
-# subtract the word from the string
-# as many times as possible and
-# if the result of the word subtracted from
-# the string is equal to the string,
-# then the string was in the word
-# for as many times as the subtraction took place
 
 def substrings(string, dictionary)
   match_hash = {}
 
   dictionary.each do |word|
-    test = word.downcase.chars - string.downcase.chars
-    if test.empty?
-      if match_hash[word]
-        match_hash[word] += 1
-      else
-        match_hash[word] = 1
-      end
+    match_count = string.downcase.scan(/#{word.downcase}/i).size
+
+    if match_count > 0
+      match_hash[word] = match_count
     end
   end
   
@@ -36,4 +20,4 @@ end
 
 match_hash = substrings(input_string, dictionary)
 
-p "matches = #{match_hash}"
+puts "matches = #{match_hash}"
