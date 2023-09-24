685db83b2e9e705bf87cab0758adeb4321f689ad
dostendite <dostendite@outlook.es>
2023-09-24 15:13:18 +0200

    Add base code for stock picker

---
+++ stock-picker/stock_picker.rb
@@ -0,0 +1,14 @@
+stock_prices = [17,3,6,9,15,8,6,1,10]
+
+def stock_picker(prices_array)
+
+  # find lowest value
+
+  # find highest value
+
+  # return index of lowest and highest values
+end
+
+days = stock_picker(stock_prices)
+
+p days
