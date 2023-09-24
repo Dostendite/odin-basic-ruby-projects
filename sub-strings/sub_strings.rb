7a34a74e1513385720e6f5a331dcd6653f0bdf76
dostendite <dostendite@outlook.es>
2023-09-24 12:05:05 +0200

    Implement almost-working solution
    It works perfectly, except it can only count words
    a single time because of the array subtraction

--- sub-strings/sub_strings.rb
+++ sub-strings/sub_strings.rb
@@ -7,25 +7,30 @@ require 'pry-byebug'
 
 print "Enter input string: "
 input_string = gets.chomp
-
 dictionary = ["below","down","go","going","horn","how","howdy",
               "it","i","low","own","part","partner","sit"]
+
+# subtract the word from the string
+# as many times as possible and
+# if the result of the word subtracted from
+# the string is equal to the string,
+# then the string was in the word
+# for as many times as the subtraction took place
 
 def substrings(string, dictionary)
   match_hash = {}
 
-  match_array = dictionary.select do |word|
-    (word.chars - string.chars).empty?
-  end
-  
-  match_array.each do |match|
-    if match_hash[match]
-      match_hash[match] += 1
-    else
-      match_hash[match] = 1
+  dictionary.each do |word|
+    test = word.downcase.chars - string.downcase.chars
+    if test.empty?
+      if match_hash[word]
+        match_hash[word] += 1
+      else
+        match_hash[word] = 1
+      end
     end
   end
-
+  
   match_hash
 end
 
