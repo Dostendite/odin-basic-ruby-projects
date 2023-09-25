8277dc398d990467c63b8d2664f10b2b7e3c9de0
dostendite <dostendite@outlook.es>
2023-09-25 13:39:09 +0200

    Clean up code & remove unnecessary variable initialization

--- bubble-sort/bubble_sort.rb
+++ bubble-sort/bubble_sort.rb
@@ -1,27 +1,12 @@
-array = [4, 1, 3, 9, 7]
+array = [4, 3, 78, 2, 0, 2]
 
 def bubble_sort(array)
-
-  # For as many times as elements in the array
   (array.size - 1).times do
-
-    # Loop through the elements
     array.each_with_index do |num, idx|
-
-      temp = 0
-
-      # If the number in array[idx]
-      # is greater than the number in array[idx + 1]
       if (array[idx + 1] != nil) && (array[idx] > array[idx + 1])
-        
-        # array[idx] is greater
-        # 1. temp = array[idx]
-        # 2. array[idx] = array[idx + 1]
-        # 3. array[idx + 1] = temp
         temp = array[idx]
         array[idx] = array[idx + 1]
         array[idx + 1] = temp
-
       end
     end
   end
