7ac619e8df4ba8aed21276bb12af5f59d401e663
Dostendite <dostendite@outlook.es>
2024-09-11 13:41:59 +0200

    Refactor: Improve bubble_sort method

--- bubble-sort/bubble_sort.rb
+++ bubble-sort/bubble_sort.rb
@@ -1,17 +1,10 @@
-array = [4, 3, 78, 2, 0, 2]
-
-def bubble_sort(array)
+def bubble_sort array
   (array.size - 1).times do
     array.each_with_index do |num, idx|
-      if (array[idx + 1] != nil) && (array[idx] > array[idx + 1])
-        temp = array[idx]
-        array[idx] = array[idx + 1]
-        array[idx + 1] = temp
+      if array[idx + 1] && array[idx] > array[idx + 1]
+        array[idx], array[idx + 1] = array[idx + 1], array[idx]
       end
     end
   end
+  array
 end
-
-puts "Unsorted array: #{array}"
-bubble_sort array
-puts "> Sorted array: #{array}"
